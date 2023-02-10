858398f816399d1fed98b603d1e7979fbf14790f
Ricky Wu <rickywu209@gmail.com>
2023-02-09 20:39:13 -0700

    tweaking the login component to apply component transitions

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+/* App.jsx */
+
+import React from "react";
+import "./App.scss";
+import { Login, Register } from "./components/login/index";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLogginActive: true
+    };
+  }
+
+  render() {
+    const { isLogginActive } = this.state;
+    const current = isLogginActive ? "Register" : "Login";
+    return (
+      <div className="App">
+        <div className="login">
+          <div className="container" ref={ref => (this.container = ref)}>
+            {isLogginActive && (
+              <Login containerRef={ref => (this.current = ref)} />
+            )}
+            {!isLogginActive && (
+              <Register containerRef={ref => (this.current = ref)} />
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
